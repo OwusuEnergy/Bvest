1d3215d54918008a99a7ad34b68925726b46131d
Christian Owusu <owusubusiness1@gmail.com>
2025-12-25 16:59:10 +0000

    remove the login with google (users will use only email)

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -22,19 +22,6 @@ export default function LoginPage() {
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-card px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
           <Input id="email" type="email" placeholder="m@example.com" required />
