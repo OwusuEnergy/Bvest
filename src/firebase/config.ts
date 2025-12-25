0abae13c448dd54fd1567c331c1ecbea45fed6f9
Christian Owusu <owusubusiness1@gmail.com>
2025-12-25 18:08:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-779383264-723d2",
+  "appId": "1:805207804298:web:1feaf19439ccdf8ed56073",
+  "apiKey": "AIzaSyAIunsqvg3t5H9JRjQlCcFUtF0OCzOUc3g",
+  "authDomain": "studio-779383264-723d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "805207804298"
+};
